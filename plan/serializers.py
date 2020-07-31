fd344cc359e799a6ea591296515463da5011bb0e
afeng <1014676950@qq.com>
2020-07-31 17:05:33 +0800

    -计划接口新增、修改、删除数据时，写入相关操作人

--- plan/serializers.py
+++ plan/serializers.py
@@ -24,6 +24,7 @@ class ProductDayPlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductDayPlan
         fields = ('equip', 'product_master', 'plan_schedule', 'pdp_product_classes_plan')
+        read_only_fields = COMMON_READ_ONLY_FIELDS
 
     @atomic()
     def create(self, validated_data):
@@ -31,27 +32,29 @@ class ProductDayPlanSerializer(serializers.ModelSerializer):
         pdp_dic['equip'] = validated_data.pop('equip')
         pdp_dic['product_master'] = validated_data.pop('product_master')
         pdp_dic['plan_schedule'] = validated_data.pop('plan_schedule')
-        instance = ProductDayPlan.objects.create(**pdp_dic)
+        pdp_dic['created_user'] = self.context['request'].user
+        # instance = ProductDayPlan.objects.create(**pdp_dic)
+        instance = super().create(pdp_dic)
         details = validated_data['pdp_product_classes_plan']
         for detail in details:
             detail_dic = dict(detail)
             detail_dic['product_day_plan'] = instance
-            ProductClassesPlan.objects.create(**detail_dic)
+            ProductClassesPlan.objects.create(**detail_dic, created_user=self.context['request'].user)
         return instance
 
     @atomic()
     def update(self, instance, validated_data):
-        print(instance)
-        print(validated_data)
         update_pcp_list = validated_data.pop('pdp_product_classes_plan', None)
+        validated_data['last_updated_user'] = self.context['request'].user
         pdp_obj = super().update(instance, validated_data)
         if update_pcp_list is None:
             return pdp_obj
         for update_pcp in update_pcp_list:
             update_pcp = dict(update_pcp)
+            update_pcp['last_updated_user'] = self.context['request'].user
             pcp_queryset = ProductClassesPlan.objects.filter(product_day_plan=instance)
             pcp_queryset.update(**update_pcp)
-        return pdp_obj
+            return pdp_obj
 
 
 class MaterialDemandedSerializer(serializers.ModelSerializer):
@@ -78,35 +81,40 @@ class ProductBatchingDayPlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductBatchingDayPlan
         fields = (
-            'equip', 'product_master', 'plan_schedule', 'sum', 'product_day_plan', 'pdp_product_batching_classes_plan')
+            'equip', 'product_master', 'plan_schedule', 'sum', 'product_day_plan',
+            'pdp_product_batching_classes_plan')
+        read_only_fields = COMMON_READ_ONLY_FIELDS
 
     @atomic()
     def create(self, validated_data):
-        print(validated_data)
+
         pdp_dic = {}
         pdp_dic['equip'] = validated_data.pop('equip')
         pdp_dic['product_master'] = validated_data.pop('product_master')
         pdp_dic['plan_schedule'] = validated_data.pop('plan_schedule')
         pdp_dic['sum'] = validated_data.pop('sum')
         pdp_dic['product_day_plan'] = validated_data.pop('product_day_plan')
-        instance = ProductBatchingDayPlan.objects.create(**pdp_dic)
+        # instance = ProductBatchingDayPlan.objects.create(**pdp_dic)
+        pdp_dic['created_user'] = self.context['request'].user
+        instance = super().create(pdp_dic)
         details = validated_data['pdp_product_batching_classes_plan']
         for detail in details:
             detail_dic = dict(detail)
             detail_dic['product_batching_day_plan'] = instance
-            ProductBatchingClassesPlan.objects.create(**detail_dic)
+            ProductBatchingClassesPlan.objects.create(**detail_dic, created_user=self.context['request'].user)
         return instance
 
     @atomic()
     def update(self, instance, validated_data):
-        print(instance)
-        print(validated_data)
+
         update_pcp_list = validated_data.pop('pdp_product_batching_classes_plan', None)
+        validated_data['last_updated_user'] = self.context['request'].user
         pdp_obj = super().update(instance, validated_data)
         if update_pcp_list is None:
             return pdp_obj
         for update_pcp in update_pcp_list:
             update_pcp = dict(update_pcp)
+            update_pcp['last_updated_user'] = self.context['request'].user
             pcp_queryset = ProductBatchingClassesPlan.objects.filter(product_batching_day_plan=instance)
             pcp_queryset.update(**update_pcp)
         return pdp_obj
@@ -127,33 +135,37 @@ class MaterialRequisitionSerializer(serializers.ModelSerializer):
         model = MaterialRequisition
         fields = (
             'material_demanded', 'count', 'plan_schedule', 'unit', 'mr_material_requisition_classes')
+        read_only_fields = COMMON_READ_ONLY_FIELDS
 
     @atomic()
     def create(self, validated_data):
-        print(validated_data)
+
         pdp_dic = {}
         pdp_dic['material_demanded'] = validated_data.pop('material_demanded')
         pdp_dic['count'] = validated_data.pop('count')
         pdp_dic['plan_schedule'] = validated_data.pop('plan_schedule')
         pdp_dic['unit'] = validated_data.pop('unit')
-        instance = MaterialRequisition.objects.create(**pdp_dic)
+        # instance = MaterialRequisition.objects.create(**pdp_dic)
+        pdp_dic['created_user'] = self.context['request'].user
+        instance = super().create(pdp_dic)
         details = validated_data['mr_material_requisition_classes']
         for detail in details:
             detail_dic = dict(detail)
             detail_dic['material_requisition'] = instance
-            MaterialRequisitionClasses.objects.create(**detail_dic)
+            MaterialRequisitionClasses.objects.create(**detail_dic, created_user=self.context['request'].user)
         return instance
 
     @atomic()
     def update(self, instance, validated_data):
-        print(instance)
-        print(validated_data)
+
         update_pcp_list = validated_data.pop('mr_material_requisition_classes', None)
+        validated_data['last_updated_user'] = self.context['request'].user
         pdp_obj = super().update(instance, validated_data)
         if update_pcp_list is None:
             return pdp_obj
         for update_pcp in update_pcp_list:
             update_pcp = dict(update_pcp)
+            update_pcp['last_updated_user'] = self.context['request'].user
             pcp_queryset = MaterialRequisitionClasses.objects.filter(material_requisition=instance)
             pcp_queryset.update(**update_pcp)
         return pdp_obj
@@ -191,18 +203,20 @@ class ProductDayPlanCopySerializer(serializers.ModelSerializer):
 
     @atomic()
     def create(self, validated_data):
-        print(validated_data)
+
         src_date = validated_data.pop('src_date')
         dst_date = validated_data.pop('dst_date')
         ps_obj = PlanSchedule.objects.filter(day_time=dst_date).first()
-        pdp_queryset = ProductDayPlan.objects.filter(plan_schedule__day_time=src_date)
+        pdp_queryset = ProductDayPlan.objects.filter(plan_schedule__day_time=src_date, delete_flag=False)
         for pdp_obj in pdp_queryset:
             instance = ProductDayPlan.objects.create(equip=pdp_obj.equip, product_master=pdp_obj.product_master,
-                                                     plan_schedule=ps_obj)
+                                                     plan_schedule=ps_obj, created_user=self.context['request'].user)
             pc_obj = ProductClassesPlan.objects.filter(product_day_plan=pdp_obj).first()
-            ProductClassesPlan.objects.create(product_day_plan=instance, sn=pc_obj.sn, num=pc_obj.num, time=pc_obj.time,
+            ProductClassesPlan.objects.create(product_day_plan=instance, sn=pc_obj.sn, num=pc_obj.num,
+                                              time=pc_obj.time,
                                               weight=pc_obj.weight, unit=pc_obj.unit,
-                                              classes_detail=pc_obj.classes_detail)
+                                              classes_detail=pc_obj.classes_detail,
+                                              created_user=self.context['request'].user)
         return instance
 
 
@@ -238,22 +252,24 @@ class ProductBatchingDayPlanCopySerializer(serializers.ModelSerializer):
 
     @atomic()
     def create(self, validated_data):
-        print(validated_data)
+
         src_date = validated_data.pop('src_date')
         dst_date = validated_data.pop('dst_date')
         ps_obj = PlanSchedule.objects.filter(day_time=dst_date).first()
-        pbdp_queryset = ProductBatchingDayPlan.objects.filter(plan_schedule__day_time=src_date)
+        pbdp_queryset = ProductBatchingDayPlan.objects.filter(plan_schedule__day_time=src_date, delete_flag=False)
         for pbdp_obj in pbdp_queryset:
             instance = ProductBatchingDayPlan.objects.create(equip=pbdp_obj.equip,
                                                              product_master=pbdp_obj.product_master,
                                                              plan_schedule=ps_obj, sum=pbdp_obj.sum,
-                                                             product_day_plan=pbdp_obj.product_day_plan)
+                                                             product_day_plan=pbdp_obj.product_day_plan,
+                                                             created_user=self.context['request'].user)
             pc_obj = ProductBatchingClassesPlan.objects.filter(product_batching_day_plan=pbdp_obj).first()
             ProductBatchingClassesPlan.objects.create(product_batching_day_plan=instance,
                                                       product_master=pc_obj.product_master, num=pc_obj.num,
                                                       time=pc_obj.time,
                                                       weight=pc_obj.weight, unit=pc_obj.unit,
-                                                      classes_detail=pc_obj.classes_detail)
+                                                      classes_detail=pc_obj.classes_detail,
+                                                      created_user=self.context['request'].user)
         return instance
 
 
@@ -289,20 +305,22 @@ class MaterialRequisitionCopySerializer(serializers.ModelSerializer):
 
     @atomic()
     def create(self, validated_data):
-        print(validated_data)
+
         src_date = validated_data.pop('src_date')
         dst_date = validated_data.pop('dst_date')
         ps_obj = PlanSchedule.objects.filter(day_time=dst_date).first()
-        mr_queryset = MaterialRequisition.objects.filter(plan_schedule__day_time=src_date)
+        mr_queryset = MaterialRequisition.objects.filter(plan_schedule__day_time=src_date, delete_flag=False)
         for mr_obj in mr_queryset:
             instance = MaterialRequisition.objects.create(material_demanded=mr_obj.material_demanded,
                                                           count=mr_obj.count,
-                                                          plan_schedule=ps_obj, unit=mr_obj.unit
+                                                          plan_schedule=ps_obj, unit=mr_obj.unit,
+                                                          created_user=self.context['request'].user
                                                           )
             pc_obj = MaterialRequisitionClasses.objects.filter(material_requisition=mr_obj).first()
             MaterialRequisitionClasses.objects.create(material_requisition=instance,
                                                       product_master=pc_obj.product_master,
                                                       weight=pc_obj.weight,
                                                       unit=pc_obj.unit,
-                                                      classes_detail=pc_obj.classes_detail)
+                                                      classes_detail=pc_obj.classes_detail,
+                                                      created_user=self.context['request'].user)
         return instance
