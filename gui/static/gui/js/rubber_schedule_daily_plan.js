1bf0c153e0ce6cfa345cd629c6eeb1cd55eb3dfa
fengqiang <330538422@qq.com>
2020-08-11 15:37:13 +0800

    配方 num改成sn

--- gui/static/gui/js/rubber_schedule_daily_plan.js
+++ gui/static/gui/js/rubber_schedule_daily_plan.js
@@ -6,12 +6,36 @@
 
             return {
                 tableDataUrl: ProductDayPlansUrl,
-                plan_data: null,
+                plan_date: null,
                 // new Date(),
                 equips: [],
                 equip_no: "",
                 stage_product_batch_no: "",
-                stage_product_batch_nos: []
+                stage_product_batch_nos: [],
+                addRubberDailyPlanDialogVisible: false,
+                rubberDailyPlanForm: {
+                    equip: null,
+                    product_batching: null,
+                    pdp_product_classes_plan: [
+                        {
+                            sn: null,
+                            plan_trains: null,
+                            time: "00:00:00",
+                            weight: ""
+                        }, {
+                            sn: null,
+                            plan_trains: null,
+                            time: "00:00:00",
+                            weight: ""
+                        }, {
+                            sn: null,
+                            plan_trains: null,
+                            time: "00:00:00",
+                            weight: ""
+                        }
+                    ]
+                },
+                productBatchings: []
             }
         },
         created: function () {
@@ -35,6 +59,7 @@
             }).then(function (response) {
 
                 app.stage_product_batch_nos = [];
+                app.productBatchings = response.data.results;
                 response.data.results.forEach(function (batching) {
 
                     if (app.stage_product_batch_nos.indexOf(batching.stage_product_batch_no) === -1) {
@@ -54,10 +79,26 @@
             },
             beforeGetData: function () {
 
-                this.getParams["plan_data"] = this.plan_data;
+                this.getParams["plan_date"] = this.plan_date;
                 this.getParams["equip_no"] = this.equip_no;
                 this.getParams["product_no"] = this.stage_product_batch_no
             },
+            addPlanClicked: function () {
+
+                this.addRubberDailyPlanDialogVisible = true
+            },
+            getPlanText: function (index) {
+
+                switch (index) {
+
+                    case 0:
+                        return "早班计划";
+                    case 1:
+                        return "中班计划";
+                    case 2:
+                        return "晚班计划"
+                }
+            }
         }
     };
     var Ctor = Vue.extend(Main);
