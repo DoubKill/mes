1451ac43109c6a495928cbf385b314f3cba5f997
hzy2020 <1105412733@qq.com>
2020-08-11 17:27:40 +0800

    -增加胶料段

--- production/serializers.py
+++ production/serializers.py
@@ -1,12 +1,14 @@
+from rest_framework import serializers
+
 from mes.base_serializer import BaseModelSerializer
 from mes.conf import COMMON_READ_ONLY_FIELDS
+from plan.models import ProductClassesPlan
 from production.models import TrainsFeedbacks, PalletFeedbacks, EquipStatus, PlanStatus, ExpendMaterial, QualityControl, \
     OperationLog
 
 
 class TrainsFeedbacksSerializer(BaseModelSerializer):
     """车次/批次产出反馈"""
-
 
     class Meta:
         model = TrainsFeedbacks
@@ -16,6 +18,13 @@ class TrainsFeedbacksSerializer(BaseModelSerializer):
 
 class PalletFeedbacksSerializer(BaseModelSerializer):
     """托盘产出反馈"""
+    stage = serializers.SerializerMethodField(read_only=True)
+
+    def get_stage(self, object):
+        plan_classes_uid = object.plan_classes_uid if object.plan_classes_uid else 0
+        productclassesplan = ProductClassesPlan.objects.filter(plan_classes_uid=plan_classes_uid).first()
+        stage = productclassesplan.product_day_plan.product_batching.stage.global_name
+        return stage if stage else ""
 
     class Meta:
         model = PalletFeedbacks
