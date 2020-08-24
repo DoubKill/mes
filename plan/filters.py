5a4c11b69824bf4f5dbbc2c7d1a2d3a5e5a1e31a
刘博文 <120@qq.com>
2020-08-24 16:08:59 +0800

    [mod] bug fix

--- plan/filters.py
+++ plan/filters.py
@@ -15,7 +15,7 @@ class ProductDayPlanFilter(django_filters.rest_framework.FilterSet):
 
 class MaterialDemandedFilter(django_filters.rest_framework.FilterSet):
     """原材料需求量过滤器"""
-    plan_date = django_filters.DateTimeFilter(field_name='plan_schedule__day_time', help_text='日期')
+    plan_date = django_filters.DateTimeFilter(field_name='work_schedule_plan__plan_schedule__day_time', help_text='日期')
     material_type = django_filters.CharFilter(field_name='material__material_type__global_name', help_text='原材料类别')
     material_name = django_filters.CharFilter(field_name='material__material_name', help_text='原材料名称',
                                               lookup_expr='icontains')
