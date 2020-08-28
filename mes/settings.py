f43d59e5fcb55f8b9983253852c8a128231f8bc0
unknown <914391033@qq.com>
2020-08-28 11:16:02 +0800

    配置oracle

--- mes/settings.py
+++ mes/settings.py
@@ -216,25 +216,46 @@ LOGGING = {
     },
 }
 
+# oracle 实例链接
+# DATABASES = {
+#     'default': {
+#     'ENGINE': 'django.db.backends.oracle',
+#     'NAME': 'IP:端口号/service_name',
+#     'USER': '用户名',
+#     'PASSWORD': '密码',
+#     }
+# }
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+# oracle实例连接
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.oracle',
+    'NAME': 'zcaj1',
+    'USER': 'zcajlj',
+    'PASSWORD': 'zcajmes2020',
+    'HOST':'10.4.10.17',
+    'PORT':'1521'
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
-            'NAME': os.getenv('DATABASE_NAME', 'mes'),  # 数据库名称
-            'USER': os.getenv('DATABASE_USERNAME', 'root'),  # 用户名
-            'PASSWORD': os.getenv('DATABASE_PASSWORD', 'mes@2020'),  # 密码
-            'HOST': os.getenv('DATABASE_HOSTNAME', '10.10.120.14'),  # HOST
-            'PORT': os.getenv('MONOCLE_API_PORT', '3306'),  # 端口
-        }
-    }
+}
+
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
+#             'NAME': os.getenv('DATABASE_NAME', 'mes'),  # 数据库名称
+#             'USER': os.getenv('DATABASE_USERNAME', 'root'),  # 用户名
+#             'PASSWORD': os.getenv('DATABASE_PASSWORD', 'mes@2020'),  # 密码
+#             'HOST': os.getenv('DATABASE_HOSTNAME', '10.10.120.14'),  # HOST
+#             'PORT': os.getenv('MONOCLE_API_PORT', '3306'),  # 端口
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
