b4d8754e1c16c24b76193db57dd48845f11c7438
李威 <liwei@gzrobot.com>
2020-10-14 17:58:45 +0800

    [add] 重构胶料库存接口

--- inventory/views.py
+++ inventory/views.py
@@ -5,9 +5,13 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import mixins
+from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
+from basics.models import GlobalCode
+from inventory.serializers import ProductInventorySerializer
+from mes.common_code import SqlClient
 from recipe.models import Material
 
 
@@ -51,17 +55,56 @@ class ProductInventory(GenericViewSet,
     # TODO
     def list(self, request, *args, **kwargs):
         # TODO 补充
-        results = [{
-            "sn": 1,
-            "material_no": "c-1MB-C9001-01",
-            "material_name": "c-1MB-C9001-01",
-            "material_type": "1MB",
-            "qty": 11,
-            "unit": "吨",
-            "unit_weight": 1,
-            "total_weight": 1,
-            "need_weight": 1,
-            "standard_flag": True,
-            "site": "立库"
-        }]
-        return Response({'results': results})
+        params = request.query_params
+        page = params.get("page", 1)
+        page_size = params.get("page_size", 10)
+        stage = params.get("stage")
+        try:
+            st = (int(page) - 1) * int(page_size) + 1
+            et = int(page) * int(page_size)
+        except:
+            raise ValidationError("page/page_size异常，请修正后重试")
+        else:
+            if st not in range(1, 99999):
+                raise ValidationError("page/page_size值异常")
+            if st not in range(1, 99999):
+                raise ValidationError("page/page_size值异常")
+        stage_list = GlobalCode.objects.filter(use_flag=True, global_type__use_flag=True,
+                                               global_type__type_name="胶料段次").values_list("global_name", flat=True)
+        if stage:
+            if stage not in stage_list:
+                raise ValidationError("胶料段次异常请修正后重试")
+            sql = f"""select * from (SELECT *, Row_Number() OVER (order by 库存索引) id FROM v_ASRS_STORE_MESVIEW where 
+                物料编码 like '%{stage}%') as temp where temp.id between {st} and {et}"""
+            sql_count = f"""select count(*) from v_ASRS_STORE_MESVIEW where 物料编码 like '%{stage}%'"""
+        else:
+            sql = f"""select * from (SELECT *, Row_Number() OVER (order by 库存索引) id FROM v_ASRS_STORE_MESVIEW) as temp  \
+                              where temp.id between {st} and {et}"""
+            sql_count = f"""select count(*) from v_ASRS_STORE_MESVIEW"""
+        sc = SqlClient(sql=sql)
+        temp = sc.all()
+        count = sc.count(sql_count)
+        # sz = ProductInventorySerializer(temp, many=True)
+        result = []
+        for instance in temp:
+            try:
+                material_type = instance[10].split("-")[1]
+            except:
+                material_type = instance[10]
+            material = instance[10].rstrip()
+            temp_dict = {
+                "sn": instance[11],
+                "material_no": material,
+                "material_name": material,
+                "material_type": material_type,
+                "qty": instance[5],
+                "unit": "kg",
+                "unit_weight": round(instance[6] / instance[5], 2),
+                "total_weight": instance[6],
+                "need_weight": instance[6],
+                "standard_flag": True,
+                "site": instance[1]
+            }
+            result.append(temp_dict)
+        sc.close()
+        return Response({'results': result, "count": count})
