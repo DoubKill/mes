4f410b9cc9482c49ddde646b39d62c3e507a3211
fengqiang <330538422@qq.com>
2020-11-05 11:09:31 +0800

    [add] 物料库存管理接口 多数据库orm相关

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -5,9 +5,10 @@ datetime: 2020/10/14
 name: 
 """
 from rest_framework import serializers
+from .models import MaterialInventory, BzFinalMixingRubberInventory, WmsInventoryStock, WmsInventoryMaterial
+
 
 class ProductInventorySerializer(serializers.Serializer):
-
     库房编号 = serializers.CharField(read_only=True)
     库房名称 = serializers.CharField(read_only=True)
     订单号 = serializers.CharField(read_only=True)
@@ -19,3 +20,55 @@ class ProductInventorySerializer(serializers.Serializer):
     车号 = serializers.CharField(read_only=True)
     库存索引 = serializers.IntegerField(read_only=True)
     物料编码 = serializers.CharField(read_only=True)
+
+
+class XBKMaterialInventorySerializer(serializers.ModelSerializer):
+    material_type = serializers.CharField(source='material.material_type.global_name', default='')
+    material_no = serializers.CharField(source='material.material_no', default='')
+
+    class Meta:
+        model = MaterialInventory
+        fields = ['material_type',
+                  'material_no',
+                  'lot_no'
+                  'container_no',
+                  'location',
+                  'qty',
+                  'unit',
+                  'unit_weight',
+                  'total_weight',
+                  'quality_status']
+
+
+class BzFinalMixingRubberInventorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BzFinalMixingRubberInventory
+        fields = ['material_type',
+                  'material_no',
+                  'lot_no',
+                  'container_no',
+                  'location',
+                  'qty',
+                  'unit',
+                  'unit_weight',
+                  'total_weight',
+                  'quality_status']
+
+
+class WmsInventoryStockSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = WmsInventoryStock
+        fields = ['material_type',
+                  'material_no',
+                  'lot_no',
+                  'container_no',
+                  'location',
+                  'qty',
+                  'unit',
+                  'unit_weight',
+                  'total_weight',
+                  'quality_status']
+
+
+
--- inventory/urls.py
+++ inventory/urls.py
@@ -17,6 +17,9 @@ router.register(r'material-inventory-view', views.MaterialInventory, basename="m
 # 胶料立库
 router.register(r'product-inventory', views.ProductInventory, basename="product-inventory")
 
+# 物料库存信息|线边库|终炼胶库|原材料库
+router.register(r'material-inventory-manage', views.MaterialInventoryManageViewSet, basename='material-inventory-manage')
+
 urlpatterns = [
     path('out-store/', OutWork.as_view()),
     path('', include(router.urls)),
