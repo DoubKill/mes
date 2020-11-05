e56770e245189f371b6050ee08b865ebd9b7367e
afeng <1014676950@qq.com>
2020-11-05 16:56:01 +0800

    [mod] 胶料但车次时间汇总

--- production/summary_views.py
+++ production/summary_views.py
@@ -204,17 +204,6 @@ class CollectTrainsFeedbacksList(ListAPIView):
     filter_class = CollectTrainsFeedbacksFilter
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
 
 class SumCollectTrains(APIView):
     """胶料单车次时间汇总最大最小平均时间"""
