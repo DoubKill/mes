398a7686717c98b1067b15188f45e1b1a3753ee9
liwei <liwei@gzrobot.com>
2020-11-11 13:25:19 +0800

    [mod] 出库计划bug修复

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -84,7 +84,7 @@ class PutPlanManagementSerializer(serializers.ModelSerializer):
                                           order_no=order_no,
                                           order_type=inventory_type,
                                           status=status,
-                                          created_user = created_user,
+                                          created_user=created_user,
                                           )
         # dps_dict = {'warehouse_info': warehouse_info,
         #             'order_no': order_no,
@@ -105,20 +105,21 @@ class PutPlanManagementSerializer(serializers.ModelSerializer):
             msg_id = validated_data['order_no']
             str_user = self.context['request'].user.username
             material_no = validated_data['material_no']
-            pallet_no = validated_data.get('pallet_no')  # 托盘号
+            pallet_no = validated_data.get('pallet_no', "20120001")  # 托盘号
             pici = 1  # 批次号
-            wegit = validated_data.get('wegit','1')
+            num = validated_data.get('need_qty', '1')
             msg_count = "1"
             location = "二层后端"
             # 发起时间
             time = validated_data.get('created_date')
-            created_time = datetime.datetime.strftime(time, '%Y%m%d %H:%M:%S')
-            WORKID = time.strftime("%Y%m%d%H%M%S", time.localtime())
-            if out_type =="指定出库":
+            created_time = time.strftime('%Y%m%d %H:%M:%S')
+            WORKID = time.strftime("%Y%m%d%H%M%S")
+            dict1 = {}
+            if out_type == "指定出库":
                 dict1 = {'WORKID': WORKID, 'MID': material_no, 'PICI': pici, 'RFID': pallet_no,
                          'STATIONID': location, 'SENDDATE': created_time}
             elif out_type == "正常出库":
-                dict1 = {'WORKID': WORKID, 'MID': material_no, 'PICI': pici, 'NUM ': wegit,
+                dict1 = {'WORKID': WORKID, 'MID': material_no, 'PICI': pici, 'NUM ': num,
                          'STATIONID': location, 'SENDDATE': created_time}
             items = []
             items.append(dict1)
@@ -133,19 +134,20 @@ class PutPlanManagementSerializer(serializers.ModelSerializer):
             # msg_count = len(json_data["items"])
             # json_data["msgConut"] = msg_count
             json_data = json.dumps(json_data, ensure_ascii=False)
-            sender = OUTWORKUploader(end_type = out_type )
+            sender = OUTWORKUploader(end_type=out_type)
             result = sender.request(msg_id, out_type, msg_count, str_user, json_data)
             if result is not None:
-                items = result['items']
-                msg = items[0]['msg']
-                msg = "TRUE"
-
+                try:
+                    items = result['items']
+                    msg = items[0]['msg']
+                except:
+                    msg = result[0]['msg']
                 warehouse_info = validated_data['warehouse_info']
                 order_no = validated_data['order_no']
                 order_type = validated_data['inventory_type']
                 created_user = self.context['request'].user.username
                 created_date = datetime.datetime.now()
-                if msg:
+                if "TRUE" in msg:
                     instance.status = 2
                     instance.last_updated_date = datetime.datetime.now()
                     instance.save()
@@ -163,7 +165,7 @@ class PutPlanManagementSerializer(serializers.ModelSerializer):
                     DeliveryPlanStatus.objects.create(warehouse_info=warehouse_info,
                                                       order_no=order_no,
                                                       order_type=order_type,
-                                                      status=status,
+                                                      status=3,
                                                       created_user=created_user,
                                                       created_date=created_date
                                                       )
@@ -214,9 +216,8 @@ class OverdueMaterialManagementSerializer(serializers.ModelSerializer):
         else:
             period_of_validity = mate_atr_obj.period_of_validity  # 保质期
             safety_inventory = mate_atr_obj.safety_inventory  # 等级
-            items = {'period_of_validity':period_of_validity,'safety_inventory':safety_inventory}
+            items = {'period_of_validity': period_of_validity, 'safety_inventory': safety_inventory}
             return items
-
 
     class Meta:
         model = MaterialInventory
