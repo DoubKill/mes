63f82f6ffa42cb0e9dcc89662e61eed7f69ca370
afeng <1014676950@qq.com>
2020-11-17 19:37:06 +0800

    [mod]删除冲突

--- quality/views.py
+++ quality/views.py
@@ -182,7 +182,7 @@ class MaterialTestOrderViewSet(mixins.CreateModelMixin,
     filter_backends = (DjangoFilterBackend,)
     permission_classes = (IsAuthenticated,)
     filter_class = MaterialTestOrderFilter
-    pagination_class = SinglePageNumberPagination
+    pagination_class = None
 
     def get_serializer_class(self):
         if self.action == 'create':
