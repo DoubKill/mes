3a6b229419bfa82d0bdc0a8db8eeec57069d35fc
liwei <liwei@gzrobot.com>
2020-11-21 10:26:19 +0800

    [mod] 配置脚本的导包路径

--- quality/tasks.py
+++ quality/tasks.py
@@ -1,6 +1,11 @@
 import os
+import sys
+
 import django
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mes.settings")
 django.setup()
 
