01fec5355460fd7ae1eb2581bc3cf3d01f00e8b7
liwei <liwei@gzrobot.com>
2020-11-21 12:17:23 +0800

    [mod] 对打印反馈接口做补偿

--- quality/views.py
+++ quality/views.py
@@ -502,6 +502,21 @@ class LabelPrintViewSet(mixins.CreateModelMixin,
             data = {}
         return Response(data)
 
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        data = request.data if request.data else {"status": 1}
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
 
 @method_decorator([api_recorder], name="dispatch")
 class DealSuggestionView(APIView):
