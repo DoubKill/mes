f63be9371df84951ab2dfb79dcaa1142ee1c8ae3
afeng <1014676950@qq.com>
2020-12-08 11:15:46 +0800

    [mod]车次报表筛选

--- production/filters.py
+++ production/filters.py
@@ -10,10 +10,11 @@ class TrainsFeedbacksFilter(django_filters.rest_framework.FilterSet):
     day_time = django_filters.DateFilter(field_name='factory_date', help_text='日期筛选')
     begin_time = django_filters.DateTimeFilter(field_name='begin_time', lookup_expr="gte", help_text='开始时间')
     end_time = django_filters.DateTimeFilter(field_name='end_time', lookup_expr="lte", help_text='结束时间')
+    operation_user = django_filters.CharFilter(field_name="operation_user", help_text="操作员")
 
     class Meta:
         model = TrainsFeedbacks
-        fields = ('plan_classes_uid', 'equip_no', 'product_no', "day_time", 'begin_time', 'end_time')
+        fields = ('plan_classes_uid', 'equip_no', 'product_no', "day_time", 'begin_time', 'end_time','operation_user')
 
 
 class PalletFeedbacksFilter(django_filters.rest_framework.FilterSet):
