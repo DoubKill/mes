901ded63e1917e34f9c92360c6a6349ecf4338ef
afeng <1014676950@qq.com>
2020-12-25 23:42:26 +0800

    [mod]备品备件导入导出修改

--- spareparts/tasks.py
+++ spareparts/tasks.py
@@ -7,30 +7,11 @@ from django.http import HttpResponse
 from io import BytesIO
 
 from rest_framework.exceptions import ValidationError
-from xlrd import xldate_as_datetime
 
 from basics.models import GlobalCode, GlobalCodeType
 from inventory.models import WarehouseInfo
 from mes import settings
 from spareparts.models import SpareType, Spare, SpareLocation, SpareLocationBinding, SpareInventory
-
-
-def spare_wrdb(filename, upload_root):
-    # 打开上传 excel 表格
-    readboot = xlrd.open_workbook(upload_root + "/" + filename)
-    sheet = readboot.sheet_by_index(0)
-    # 获取excel的行和列
-    nrows = sheet.nrows  # 行
-    ncols = sheet.ncols  # 列
-    for i in range(1, nrows):
-        row = sheet.row_values(i)
-        st_obj = SpareType.objects.filter(name=row[1]).first()
-        if not st_obj:
-            st_obj = SpareType.objects.create(no=row[1], name=row[1])
-        s_obj = Spare.objects.filter(no=row[2]).first()
-        if not s_obj:
-            Spare.objects.create(no=row[2], name=row[3], type=st_obj, unit=row[7], upper=row[6], lower=row[5],
-                                 cost=row[4])
 
 
 def spare_inventory_wrdb(filename, upload_root):
@@ -63,7 +44,7 @@ def spare_inventory_wrdb(filename, upload_root):
         if not s_obj:
             s_obj = Spare.objects.create(no=row[2], name=row[3], type=st_obj, unit=row[11], upper=row[10], lower=row[9],
                                          cost=row[7])
-        gc_obj = GlobalCode.objects.filter(global_no=row[5]).first()
+        gc_obj = GlobalCode.objects.filter(global_name=row[5]).first()
         if not gc_obj:
             gct_obj = GlobalCodeType.objects.filter(type_name='备品备件类型').first()
             gc_obj = GlobalCode.objects.create(global_type=gct_obj, global_no=row[5], global_name=row[5])
@@ -81,7 +62,7 @@ def spare_inventory_wrdb(filename, upload_root):
                                       warehouse_info=whi_obj)
 
 
-def spare_upload(request, type):
+def spare_upload(request):
     # 根name取 file 的值
     file = request.FILES.get('file')
     # 创建upload文件夹
@@ -100,45 +81,9 @@ def spare_upload(request, type):
 
     # # 写入 mysql
     try:
-        if type == 1:
-            spare_wrdb(file.name, upload_root)
-        elif type == 2:
-            spare_inventory_wrdb(file.name, upload_root)
-
+        spare_inventory_wrdb(file.name, upload_root)
     except Exception as e:
         raise ValidationError(f'异常信息{e}')
-
-
-def spare_template():
-    """备品备件基本信息导入模板"""
-    response = HttpResponse(content_type='application/vnd.ms-excel')
-    filename = '备品备件基本信息'
-    response['Content-Disposition'] = 'attachment;filename= ' + filename.encode('gbk').decode('ISO-8859-1') + '.xls'
-    # 创建工作簿
-    style = xlwt.XFStyle()
-    style.alignment.wrap = 1
-    ws = xlwt.Workbook(encoding='utf-8')
-
-    # 添加第一页数据表
-    w = ws.add_sheet('备品备件基本信息')  # 新建sheet（sheet的名称为"sheet1"）
-    # for j in [1, 4, 5, 7]:
-    #     first_col = w.col(j)
-    #     first_col.width = 256 * 20
-    # 写入表头
-    w.write(0, 0, u'No')
-    w.write(0, 1, u'物料类型')
-    w.write(0, 2, u'物料编码')
-    w.write(0, 3, u'物料名称')
-    w.write(0, 4, u'单价（元）')
-    w.write(0, 5, u'安全库存下限')
-    w.write(0, 6, u'安全库存上限')
-    w.write(0, 7, u'单位')
-    output = BytesIO()
-    ws.save(output)
-    # 重新定位到开始
-    output.seek(0)
-    response.write(output.getvalue())
-    return response
 
 
 def spare_inventory_template():
