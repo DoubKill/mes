dd5d8efaa380bf64b5b20cc260388f6882e8dc7e
fengqiang <330538422@qq.com>
2020-12-28 12:58:41 +0800

    [mod] 无小料胶料配方bug

--- plan/signals.py
+++ plan/signals.py
@@ -1,5 +1,8 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+from recipe.models import ProductBatching
 from .models import ProductClassesPlan, BatchingClassesPlan
 
 
@@ -16,11 +19,14 @@ def product_classes_plan_save_handler(sender, **kwargs):
                 weigh_cnt_type__weigh_batching__product_batching=product_classes_plan.product_batching
             ).update(delete_flag=True)
     else:
-        if product_classes_plan.product_batching.weighbatching:  # 如果计划关联胶料配方有小料配方
-            for cnt_type in product_classes_plan.product_batching.weighbatching.weighcnttype_set.all():
-                batching_classes_plan, created = BatchingClassesPlan.objects.get_or_create(
-                    work_schedule_plan=product_classes_plan.work_schedule_plan,
-                    weigh_cnt_type=cnt_type)
-                if not created and batching_classes_plan.delete_flag:
-                    batching_classes_plan.delete_flag = False
-                    batching_classes_plan.save()
+        try:
+            if product_classes_plan.product_batching.weighbatching:  # 如果计划关联胶料配方有小料配方
+                for cnt_type in product_classes_plan.product_batching.weighbatching.weighcnttype_set.all():
+                    batching_classes_plan, created = BatchingClassesPlan.objects.get_or_create(
+                        work_schedule_plan=product_classes_plan.work_schedule_plan,
+                        weigh_cnt_type=cnt_type)
+                    if not created and batching_classes_plan.delete_flag:
+                        batching_classes_plan.delete_flag = False
+                        batching_classes_plan.save()
+        except ObjectDoesNotExist as e:
+            pass
