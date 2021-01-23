9200779a1d651a86cd88f71c0e52a5c9e44b9726
liwei <liwei@gzrobot.com>
2021-01-23 11:22:49 +0800

    [mod] 指定出库展示所有货位

--- inventory/views.py
+++ inventory/views.py
@@ -307,24 +307,23 @@ class MaterialInventoryManageViewSet(viewsets.ReadOnlyModelViewSet):
         model = self.divide_tool(self.MODEL)
         queryset = None
         material_type, container_no, material_no, order_no, location = self.get_query_params()
+        quality_status = self.request.query_params.get('quality_status', None)
         if model == XBMaterialInventory:
             queryset = model.objects.all()
         elif model == BzFinalMixingRubberInventory:
             # 出库计划弹框展示的库位数据需要更具库位状态进行筛选其他页面不需要
-            if self.request.query_params.get("location_status"):
-                queryset = model.objects.using('bz').filter(location_status="有货货位")
-            else:
-                queryset = model.objects.using('bz').all()
-            quality_status = self.request.query_params.get('quality_status', None)
+            # if self.request.query_params.get("location_status"):
+            #     queryset = model.objects.using('bz').filter(location_status=self.request.query_params.get("location_status"))
+            # else:
+            queryset = model.objects.using('bz').all()
             if quality_status:
                 queryset = queryset.filter(quality_level=quality_status)
         elif model == BzFinalMixingRubberInventoryLB:
             # 出库计划弹框展示的库位数据需要更具库位状态进行筛选其他页面不需要
-            if self.request.query_params.get("location_status"):
-                queryset = model.objects.using('lb').filter(location_status="有货货位")
-            else:
-                queryset = model.objects.using('lb').all()
-            quality_status = self.request.query_params.get('quality_status', None)
+            # if self.request.query_params.get("location_status"):
+            #     queryset = model.objects.using('lb').filter(location_status=self.request.query_params.get("location_status"))
+            # else:
+            queryset = model.objects.using('lb').all()
             if quality_status:
                 queryset = queryset.filter(quality_level=quality_status)
         if queryset:
