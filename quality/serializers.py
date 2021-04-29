f831ea8abcbed69704718bcd57dfaa0b6e0cebbb
刘博文 <120@qq.com>
2021-04-29 17:31:27 +0800

    [mod]补充pallet_no；

--- quality/serializers.py
+++ quality/serializers.py
@@ -393,6 +393,7 @@ class MaterialDealResultListSerializer(BaseModelSerializer):
         ret['test'] = {'test_status': '复检' if test_results.filter(test_times__gt=1).exists() else '正常',
                        'test_factory_date': str(last_test_result.test_factory_date),
                        'test_class': test_order_data.production_class,
+                       'pallet_no': pallet_data.pallet_no,
                        'test_user': None if not test_order_data.created_user else test_order_data.created_user.username}
         product_time = instance.production_factory_date
         material_detail = MaterialAttribute.objects.filter(material__material_no=pallet_data.product_no).first()
