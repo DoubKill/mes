14701985d2a3a07a34ff7131b3c05eaaa6882d8e
kevin <771411855@qq.com>
2021-08-11 17:19:26 +0800

    [mod]使用re取出中文括号

--- terminal/serializers.py
+++ terminal/serializers.py
@@ -1,4 +1,5 @@
 import random
+import re
 from datetime import datetime, timedelta
 
 import requests
@@ -368,8 +369,9 @@ class WeightPackageLogCreateSerializer(serializers.ModelSerializer):
         # 打印数量判断
         if print_count <= 0 or print_count > package_count or not isinstance(print_count, int):
             raise serializers.ValidationError('打印张数需小于等于配置数量')
-        weight_type_record = WeighCntType.objects.filter(product_batching__stage_product_batch_no=product_no.split('(')[0],
-                                                         package_type=1, product_batching__dev_type__category_no=dev_type)
+        weight_type_record = WeighCntType.objects.filter(product_batching__stage_product_batch_no=re.split(r'\(|\（', product_no)[0],
+                                                         package_type=1, product_batching__dev_type__category_no=dev_type,
+                                                         product_batching__used_type=4)
         if weight_type_record:
             attrs['material_no'] = weight_type_record.first().name
             attrs['material_name'] = attrs['material_no']
