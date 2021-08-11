72a6a20d8f20ee8e095399bd0ca367a775c4b16e
刘博文 <120@qq.com>
2021-08-11 12:45:01 +0800

    [mod] 胶片库快检信息更新；

--- mdr_send_bz.py
+++ mdr_send_bz.py
@@ -6,8 +6,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mes.settings")
 django.setup()
 from tasks import update_wms_kjjg
 import datetime
-from django.db.models import Q, Max
-from inventory.models import BzFinalMixingRubberInventory, MaterialInventory
+from inventory.models import BzFinalMixingRubberInventory, BzFinalMixingRubberInventoryLB
 from mes.common_code import order_no
 from production.models import PalletFeedbacks
 from quality.models import MaterialTestOrder, MaterialDealResult
@@ -20,22 +19,20 @@ logger = logging.getLogger('send_log')
 
 
 def send_bz():
-    # 5、向北自接口发送数据
-    # 5.1、先判断库存和线边库里有没有数据
     deal_results = MaterialDealResult.objects.exclude(
         update_store_test_flag=1).filter(send_count__lt=SEND_COUNT)
     for mdr_obj in deal_results:
         pfb_obj = PalletFeedbacks.objects.filter(lot_no=mdr_obj.lot_no).first()
-        bz_obj = BzFinalMixingRubberInventory.objects.using('bz').filter(
-            Q(container_no=pfb_obj.pallet_no) | Q(lot_no=mdr_obj.lot_no)).last()
-        mi_obj = MaterialInventory.objects.filter(Q(container_no=pfb_obj.pallet_no) | Q(lot_no=mdr_obj.lot_no)).last()
-        # 5.2、一个库里有就发给北自，没有就不发给北自
-        if bz_obj or mi_obj:
+        bz_obj = BzFinalMixingRubberInventory.objects.using('bz').filter(lot_no=mdr_obj.lot_no).last()
+        ware = '混炼'
+        if not bz_obj:
+            bz_obj = BzFinalMixingRubberInventoryLB.objects.using('lb').filter(lot_no=mdr_obj.lot_no).last()
+            ware = "终炼"
+        if bz_obj:
             try:
                 # 4、update_store_test_flag这个字段用choise 1对应成功 2对应失败 3对应库存线边库都没有
                 msg_ids = order_no()
                 mto_obj = MaterialTestOrder.objects.filter(lot_no=mdr_obj.lot_no).first()
-                pfb_obj = PalletFeedbacks.objects.filter(lot_no=mdr_obj.lot_no).first()
                 item = []
                 item_dict = {"WORKID": str(int(msg_ids) + 1),
                              "MID": mto_obj.product_no,
@@ -45,7 +42,7 @@ def send_bz():
                              "SENDDATE": datetime.datetime.now().strftime('%Y%m%d %H:%M:%S')}
                 item.append(item_dict)
                 # 向北自发送数据
-                res = update_wms_kjjg(items=item)
+                res = update_wms_kjjg(ware, items=item)
                 if not res:  # res为空代表成功
                     mdr_obj.update_store_test_flag = 1
                     mdr_obj.save()
