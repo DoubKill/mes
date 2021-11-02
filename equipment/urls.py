94d6ac1a1697eb1ce14bdd10b1c3523471485d6b
fivenian1 <402470556@qq.com>
2021-11-02 18:46:06 +0800

    [add] 备件出入库管理

--- equipment/urls.py
+++ equipment/urls.py
@@ -85,12 +85,6 @@ router.register('equip-maintenance-area-settings', EquipMaintenanceAreaSettingVi
 
 # 设备作业项目标准定义
 router.register('equip-job-item-standard', EquipJobItemStandardViewSet)
-#
-# # 备件库库区
-# router.register('equip-warehouse-area', EquipWarehouseAreaViewSet)
-
-# # 备件库库位
-# router.register('equip-warehouse-location', EquipWarehouseLocationViewSet)
 
 # 设备维护作业标准定义
 router.register('equip_maintenance_standard', EquipMaintenanceStandardViewSet)
