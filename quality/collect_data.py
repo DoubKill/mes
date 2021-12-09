1ab897b6ec2a7794fd417173fc1215a43523b3a8
刘博文 <120@qq.com>
2021-12-09 16:14:05 +0800

    [add] 诺甲系统数据采集；

--- quality/collect_data.py
+++ quality/collect_data.py
@@ -1,172 +1,154 @@
 
+"""
+    根据机台检测计划，自动采集诺甲系统质检数据
+"""
+import datetime
+import json
 import os
 import sys
 import uuid
-from datetime import datetime
 
 import django
-from django.db.models import Max
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mes.settings")
 django.setup()
 
-from production.models import PalletFeedbacks
-from quality.models import MaterialTestOrder, MaterialTestResult, TestType, MaterialTestMethod, \
-    MaterialDataPointIndicator
-from basics.models import WorkSchedulePlan
-from quality.models import ZCKJConfig
-
 import pymssql
 import logging
 logger = logging.getLogger('quality_log')
 
+from quality.models import ZCKJConfig, ProductTestPlanDetail, ProductTestPlan, MaterialDataPointIndicator, \
+    MaterialTestResult, MaterialTestMethod, MaterialTestOrder, ProductReportEquip
+from production.models import PalletFeedbacks
 
-def get_min_max_id(server, user, password, database, test_date):
-    sql = """select min(RID), max(RID) from ResultInfo where TestDate>'{}';""".format(test_date)
+
+def get_rids(server, user, password, database, machine_no, rid, test_time):
+    sql = """select 
+    RID 
+    from ResultInfo 
+    where MachineNo='{}' and RID>{} and TestDate>'{}' order by RID;""".format(machine_no, rid, test_time)
     conn = pymssql.connect(server, user, password, database)
     cur = conn.cursor()
     cur.execute(sql)
     data = cur.fetchall()
-    return data[0][0], data[0][1]
+    return [i[0] for i in data]
 
 
 def main():
-    for config in ZCKJConfig.objects.all():
-        max_test_date = MaterialTestResult.objects.filter(origin=config.id).aggregate(
-            max_test_date=Max('test_factory_date'))['max_test_date']
-        if not max_test_date:
-            max_test_date = '2020-11-22 00:00:00'
-        else:
-            max_test_date = datetime.strftime(max_test_date, "%Y-%m-%d %H:%M:%S")
-        logger.info('ip:{}， max_test_date: {}'.format(config.server, max_test_date))
-        server = config.server
-        user = config.user
-        password = config.password
-        name = config.name
-        try:
-            min_id, max_id = get_min_max_id(server, user, password, name, max_test_date)
-        except Exception:
-            logger.error('connect database:{} error !'.format(server))
-            continue
-        if not min_id:
-            continue
-        while max_id >= min_id+1:
-            sql = """select
-                   r.DValue,
-                   c.CompoundName,
-                   rf.TestClass,
-                   rf.TestGroup,
-                   r.DataName,
-                   mtm.MethodName,
-                   rf.Result,
-                   rf.Shift,
-                   rf.MixDate,
-                   rf.MixerNo,
-                   rf.TestDate,
-                   rf.TestNo,
-                   mt.MachineTypename,
-                   rf.CompoundCode,
-                   tt.TypeName
-                from ResultInfo rf
-            inner join SubTest st on st.RID=rf.RID
-            inner join Result r on rf.RID=r.RID
-            inner join Compound c on c.id=rf.CompoundID
-            inner join MainTestMethod mtm on mtm.id=rf.MainMethodID
-            inner join TestType tt on tt.ID=mtm.TestTypeID
-            inner join MachineType1 mt on mt.ID=tt.MachineTypeID
-            where 
-                rf.RID>={} 
-                and rf.RID<={} 
-                and rf.TestGroup!='' 
-                and rf.MixerNo in ('Z01', 'Z02','Z03','Z04','Z05',
-                                   'Z06','Z07','Z08','Z09','Z10',
-                                   'Z11', 'Z12','Z13','Z14','Z15')
-                and rf.TestClass!='';""".format(str(min_id), str(min_id+1000))
-            conn = pymssql.connect(server, user, password, name)
-            cur = conn.cursor()
-            cur.execute(sql)
-            data = cur.fetchall()
-            for item in data:
-                value = item[0]
-                product_no = item[1].strip(' ')  # 胶料代码
-                production_class = item[2].strip(' ')  # 生产班次
-                data_point_name = item[4].strip(' ')  # 数据点
-                method_name = item[5].strip(' ')  # 试验方法名称
-                result = item[6].strip(' ')  # 结果
-                trains = int(item[7])  # 车次
-                d = []
-                for a in item[8].strip(' ').split('/'):
-                    d.append(a.zfill(2))
-                product_date = '-'.join(d)  # 生产日期
-                equip_no = item[9].strip(' ')  # 设备编号
-                test_date = item[10]  # 试验日期
-                test_times = item[11]  # 试验次数
-                try:
-                    interval = int(item[13].strip(' '))
-                except Exception:
-                    interval = 1
-                test_group = item[3].strip(' ')  # 试验班组
-                test_type_name = item[-1].strip(' ')  # 检测类型
+    for config in ZCKJConfig.objects.filter(use_flag=True):
 
-                # 根据机器名称找到指标点
-                test_type = TestType.objects.filter(name=test_type_name).first()
-                if not test_type:
-                    logger.error('no test type:{}'.format(test_type_name))
+        for sub_machine in config.sub_machines.all():
+            server = config.server
+            user = config.user
+            password = config.password
+            name = config.name
+
+            # 获取检测机台当前正在进行中的检测计划
+            equip_test_plan = ProductTestPlan.objects.filter(test_equip__no=sub_machine.machine_no,
+                                                             status=1).order_by('id').last()
+
+            if not equip_test_plan:
+                logger.error('机台：{}，暂无正在检测的计划'.format(sub_machine.machine_no))
+                continue
+
+            try:
+                rids = get_rids(server,
+                                user,
+                                password,
+                                name,
+                                sub_machine.nj_machine_no,
+                                sub_machine.max_rid,
+                                equip_test_plan.created_date.strftime('%Y-%m-%d %H:%M:%S'))
+            except Exception:
+                logger.error('connect database:{} error !'.format(server))
+                continue
+
+            method_name = equip_test_plan.test_method_name
+            indicator_name = equip_test_plan.test_indicator_name
+            test_times = equip_test_plan.test_times
+            test_group = equip_test_plan.test_group
+            test_class = equip_test_plan.test_classes
+
+            # 给检测任务绑定检测值
+            for rid in rids:
+                # 获取当前检测任务
+                current_test_detail = ProductTestPlanDetail.objects.filter(test_plan=equip_test_plan,
+                                                                           value__isnull=True
+                                                                           ).order_by('id').first()
+                if not current_test_detail:
+                    logger.error('检测机台：{}，检测任务已全部完成'.format(sub_machine.machine_no))
                     continue
-                else:
-                    indicator_name = test_type.test_indicator.name
+                sql = """select Dvalue,DataName from Result where RID={};""".format(rid)
+                conn = pymssql.connect(server, user, password, name)
+                cur = conn.cursor()
+                cur.execute(sql)
+                data = cur.fetchall()
 
-                # 根据班组找班次（找到的不一定对）
-                schedule_plan = WorkSchedulePlan.objects.filter(
-                    plan_schedule__work_schedule__schedule_name='三班两运转',
-                    plan_schedule__day_time=product_date,
-                    classes__global_name=production_class
-                ).first()
-                if schedule_plan:
-                    group = schedule_plan.group.global_name
-                else:
-                    group = 'A'
+                # 更新当前检测任务结果值
+                test_results = {item[1].strip(' '): item[0] for item in data}
+                current_test_detail.value = json.dumps(test_results)
+                current_test_detail.save()
 
-                # 关键看能不能找到托盘反馈数据
-                for i in range(trains, trains+interval):
-                    # 根据机台编号、胶料代码、班次、日期找托盘lot_no
-                    pallet = PalletFeedbacks.objects.filter(
-                        equip_no=equip_no,
-                        product_no=product_no,
-                        classes=production_class,
-                        factory_date=product_date,
-                        begin_trains__lte=i,
-                        end_trains__gte=i
-                    ).first()
+                # 卡片判级
+                product_no = current_test_detail.product_no  # 胶料编码
+                production_class = current_test_detail.production_classes  # 班次
+                production_group = current_test_detail.production_group  # 班组
+                equip_no = current_test_detail.equip_no  # 机台
+                factory_date = current_test_detail.factory_date  # 工厂日期
+
+                material_test_method = MaterialTestMethod.objects.filter(
+                    material__material_no=product_no,
+                    test_method__name=method_name).first()
+                if not material_test_method:
+                    continue
+                test_data_points = material_test_method.data_point.values_list('name', flat=True)
+
+                for train in range(current_test_detail.actual_trains,
+                                   current_test_detail.actual_trains + equip_test_plan.test_interval):
+
+                    if train == current_test_detail.actual_trains:
+                        pallet = PalletFeedbacks.objects.filter(
+                            lot_no=current_test_detail.lot_no).first()
+                    else:
+                        pallet = PalletFeedbacks.objects.filter(
+                            equip_no=equip_no,
+                            product_no=product_no,
+                            classes=production_class,
+                            factory_date=factory_date,
+                            begin_trains__lte=train,
+                            end_trains__gte=train
+                        ).first()
                     if not pallet:
-                        logger.error("cant find pallet data, 设备：{}， 胶料：{}， 班次：{}， 日期：{}， 车次：{}".format(
-                            equip_no, product_no, production_class, product_date, i))
                         continue
                     lot_no = pallet.lot_no
-                    test_order = MaterialTestOrder.objects.filter(lot_no=lot_no,
-                                                                  actual_trains=i).first()
+
+                    # 车次检测单
+                    test_order = MaterialTestOrder.objects.filter(
+                        lot_no=lot_no, actual_trains=train).first()
                     if not test_order:
                         test_order = MaterialTestOrder.objects.create(
-                            lot_no=lot_no,
+                            lot_no=current_test_detail.lot_no,
                             material_test_order_uid=uuid.uuid1(),
-                            actual_trains=i,
+                            actual_trains=train,
                             product_no=product_no,
                             plan_classes_uid=pallet.plan_classes_uid,
                             production_class=production_class,
-                            production_group=group,
+                            production_group=production_group,
                             production_equip_no=equip_no,
-                            production_factory_date=product_date
+                            production_factory_date=factory_date
                         )
-                    # 由MES判断检测结果
-                    material_test_method = MaterialTestMethod.objects.filter(
-                        material__material_no=product_no,
-                        test_method__name=method_name,
-                        test_method__test_type__test_indicator__name=indicator_name,
-                        data_point__name=data_point_name,
-                        data_point__test_type__test_indicator__name=indicator_name).first()
-                    if material_test_method:
+
+                    # 对数据点进行判级
+                    for item in data:
+                        value = item[0]
+                        data_point_name = item[1].strip(' ')
+                        if data_point_name not in test_data_points:
+                            continue
+
+                        # 获取判级标准
                         indicator = MaterialDataPointIndicator.objects.filter(
                             material_test_method=material_test_method,
                             data_point__name=data_point_name,
@@ -179,34 +161,35 @@ def main():
                         else:
                             mes_result = '三等品'
                             level = 2
-                    else:
-                        logger.error('该胶料{}实验方法{}不存在!'.format(product_no, method_name))
-                        continue
-                    if not MaterialTestResult.objects.filter(
-                            material_test_order=test_order,
-                            test_times=test_times,
-                            data_point_name=data_point_name,
-                            test_indicator_name=indicator_name,
-                            origin=config.id).exists():
+
+                        # 创建数据点检测结果
                         MaterialTestResult.objects.create(
                             material_test_order=test_order,
-                            test_factory_date=test_date,
+                            test_factory_date=datetime.datetime.now(),
                             value=value,
                             test_times=test_times,
                             data_point_name=data_point_name,
                             test_method_name=method_name,
                             test_indicator_name=indicator_name,
-                            result=result,
+                            result=mes_result,
                             mes_result=mes_result,
-                            machine_name=indicator_name+'仪',
+                            machine_name=equip_test_plan.test_equip.no,
                             test_group=test_group,
                             level=level,
-                            test_class=production_class,
-                            origin=config.id,
-                            is_judged=material_test_method.is_judged
+                            test_class=test_class,
+                            is_judged=material_test_method.is_judged,
+                            created_user=equip_test_plan.created_user
                         )
-            conn.close()
-            min_id += 1000
+
+            # 判断该机台计划是否全部检测完成
+            if equip_test_plan.product_test_plan_detail.filter(
+                    value__isnull=False).count() == equip_test_plan.product_test_plan_detail.count():
+                equip_test_plan.status = 2
+                equip_test_plan.save()
+
+            if rids:
+                sub_machine.max_rid = rids[-1]
+                sub_machine.save()
 
 
 if __name__ == '__main__':
