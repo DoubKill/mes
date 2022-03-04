ee03ea84a933fcce9216a6ea765a50cacf19eea5
kevin <771411855@qq.com>
2022-03-04 13:13:10 +0800

    [mod]自动派单成功消息可跳转

--- equipment/task.py
+++ equipment/task.py
@@ -310,16 +310,25 @@ class AutoDispatch(object):
             content.update({'title': f"系统自动派发{order.work_type}工单成功，请尽快处理！"})
             self.ding_api.send_message([per.get('ding_uid')], content, order_id=order.id, inspection=inspection)
             # 派单成功发送消息到设备群聊
-            msg = f"系统自动派发设备工单成功，请尽快处理！\n工单编号:{order.work_order_no}\n机台:{order.equip_no}\n故障原因:{fault_name}\n重要程度:{order.importance_level}\n指派人:系统自动\n被指派人:{per['username']}\n指派时间:{now_date}"
+            message_url = f"eapp://pages/repairOrder/repairOrder?id={order.id}" + ("&isInspection=true" if inspection else "") + "&isView=1"
+            msg_to_group = {
+                "msgtype": "link",
+                "link": {
+                    "text": f"系统自动派发设备工单成功，请尽快处理！\n工单编号:{order.work_order_no}\n机台:{order.equip_no}\n故障原因:{fault_name}\n重要程度:{order.importance_level}\n指派人:系统自动\n被指派人:{per['username']}\n指派时间:{now_date}",
+                    "title": "系统自动派发设备工单成功，请尽快处理！",
+                    "picUrl": "",
+                    "messageUrl": message_url
+                }
+            }
             url = self.get_group_url()
-            send_ding_msg(url=url, secret=self.group_secret, msg=msg, isAtAll=False)
+            send_ding_msg(url=url, secret=self.group_secret, msg=msg_to_group, isAtAll=False)
             logger.info(f"系统派单[{order.work_type}]-系统自动派单成功: {order.work_order_no}, 被指派人:{per['username']}")
             continue
 
         if len(processing_person) == len(working_persons):
             # 所有人都在忙, 派单失败, 钉钉消息推送给上级
             content.update({'title': f"所有人员均有工单在处理, 系统自动派单失败！"})
-            logger.info(f'系统派单[{order.work_type}]: 系统自动派单失败, 可选人员:{working_persons}, 正在维修人员:{processing_person}')
+            logger.info(f'系统派单[{order.work_type}]: 系统自动派单失败: {order.work_order_no}, 可选人员:{working_persons}, 正在维修人员:{processing_person}')
             return [order.work_type, leader_ding_uid]
         return f'系统派单[{order.work_type}]: 完成一次定时派单处理'
 
