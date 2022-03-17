647c758279bd8b393cd841df4292e680d06ead40
fivenian1 <402470556@qq.com>
2022-03-17 14:01:48 +0800

    [mod] 备件入库过滤

--- equipment/views.py
+++ equipment/views.py
@@ -2469,12 +2469,13 @@ class EquipWarehouseOrderViewSet(ModelViewSet):
                 queryset = self.filter_queryset(self.get_queryset().filter(status__in=[4, 5, 6]))
             else:
                 queryset = self.filter_queryset(self.get_queryset())
-            page = self.paginate_queryset(queryset.distinct())
+            queryset = list(set(queryset))
+            page = self.paginate_queryset(queryset)
             if page is not None:
                 serializer = self.get_serializer(page, many=True)
                 return self.get_paginated_response(serializer.data)
 
-            serializer = self.get_serializer(queryset.distinct(), many=True)
+            serializer = self.get_serializer(queryset, many=True)
             return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
