35f525bdecab9bb1417b4ed81787786f777e468e
刘博文 <120@qq.com>
2022-04-19 15:52:54 +0800

    [add] 原材料送检；

--- quality/update_wms_test_result.py
+++ quality/update_wms_test_result.py
@@ -2,7 +2,7 @@
 """
     更新wms物料检测结果信息
 """
-
+import json
 import os
 import sys
 from datetime import datetime, timedelta
@@ -16,7 +16,7 @@ sys.path.append(BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mes.settings")
 django.setup()
 
-from quality.models import ExamineMaterial
+from quality.models import ExamineMaterial, MaterialInspectionRegistration
 from inventory.models import WmsNucleinManagement, WMSExceptHandle
 
 from mes.conf import WMS_URL
@@ -57,6 +57,45 @@ def main():
             logger.error('wms error msg: {}'.format(r.get('msg')))
         m.status = 2 if status == 1 else 3
         m.save()
+
+    # 更新总部检测结果
+    inspection_materials = MaterialInspectionRegistration.objects.filter(quality_status='待检')
+    inspection_url = 'http://10.1.10.136/zcxjws_web/zcxjws/pc/zc/getCkdtm.io'
+    for inspection_material in inspection_materials:
+        try:
+            json_data = {"fac": "AJ1", "tmh": inspection_material.tracking_num}
+            ret = requests.post(inspection_url, json=json_data, timeout=10)
+        except Exception:
+            logger.error('查询总部质检结果失败，网络异常！')
+            continue
+        inspection_data = json.loads(ret.text)
+        obj = inspection_data.get('obj')
+        if not obj:
+            continue
+        else:
+            zjzt = obj[0].get('zjzt')
+            if not zjzt:
+                continue
+            check_result = None
+            if zjzt == '合格品':
+                quality_status = '合格'
+                check_result = 1
+            elif zjzt == '待检品':
+                quality_status = '待检'
+            else:
+                quality_status = '不合格'
+                check_result = 2
+            inspection_material.quality_status = quality_status
+            inspection_material.save()
+
+            # 将总部检测结果更新到立库
+            if check_result:
+                inspection_data_list = [{
+                            "BatchNo": inspection_material.batch,
+                            "MaterialCode": inspection_material.material_no,
+                            "CheckResult": check_result
+                        }]
+                update_wms_quality_result(inspection_data_list)
 
     # 核酸检测锁定（设定不合格）
     data_list = [{
