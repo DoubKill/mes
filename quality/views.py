ed0658498d23723ca5a972abc31c1d6f45b8214c
刘博文 <120@qq.com>
2022-04-22 13:28:25 +0800

    [mod] 不合格处理增加加硫和无硫过滤；

--- quality/views.py
+++ quality/views.py
@@ -2476,6 +2476,26 @@ class UnqualifiedPalletFeedBackListView(ListAPIView):
     filter_backends = (DjangoFilterBackend, )
     filter_fields = ('product_no', 'factory_date', 'classes', 'equip_no', 'is_deal')
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        sulfur_flag = self.request.query_params.get('sulfur_flag')
+        if sulfur_flag:
+            if sulfur_flag == '加硫':
+                queryset = queryset.filter(Q(product_no__icontains='-FM') |
+                                           Q(product_no__icontains='-RFM') |
+                                           Q(product_no__icontains='-RE'))
+            else:
+                queryset = queryset.exclude(Q(product_no__icontains='-FM') |
+                                            Q(product_no__icontains='-RFM') |
+                                            Q(product_no__icontains='-RE'))
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
 
 @method_decorator([api_recorder], name='dispatch')
 class ProductTestStaticsView(APIView):
