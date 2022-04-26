fda717aa083250185d83a1acf576535d3cd2edc5
fivenian1 <402470556@qq.com>
2022-04-26 10:33:24 +0800

    [mod] 钉钉考勤

--- production/views.py
+++ production/views.py
@@ -2466,11 +2466,11 @@ class SummaryOfWeighingOutput(APIView):
                     for name, section in names.items():
                         key = f"{name}_{day}_{classes}_{section}"
                         work_time = work_times.get(f'{day}-{classes}-{equip_no}').get(name)
-                        # 车数计算：当天产量 / 12小时 * 实际工作时间，向上取整数
+                        # 车数计算：当天产量 / 12小时 * 实际工作时间
                         if user_result.get(key):
-                            user_result[key][equip_no] = user_result[key].get(equip_no, 0) + ceil(item['count'] / 12 * work_time)
+                            user_result[key][equip_no] = user_result[key].get(equip_no, 0) + item['count'] / 12 * work_time
                         else:
-                            user_result[key] = {equip_no: ceil(item['count'] / 12 * work_time)}
+                            user_result[key] = {equip_no: item['count'] / 12 * work_time}
             result.append(dic)
         for key, value in user_result.items():  # value {'F03': 109, 'F02': 100,},
             name, day, classes, section = key.split('_')
@@ -2960,7 +2960,7 @@ class PerformanceSummaryView(APIView):
                         else:
                             unit = price_dic.get(f"{equip_type}_{state}").get('dj')
                         user_dic[key][f"{state}_dj_qty"] = user_dic[key].get(f"{state}_dj_qty", 0) + \
-                                                           ceil(item['actual_trains'] / 12 * work_time)
+                                                           item['actual_trains'] / 12 * work_time
                         user_dic[key][f"{state}_dj_unit"] = unit
                     else:
                         work_time = user_dic[key]['actual_time']
@@ -2969,7 +2969,7 @@ class PerformanceSummaryView(APIView):
                         else:
                             unit = price_dic.get(f"{equip_type}_{state}").get('pt')
                         user_dic[key][f"{state}_pt_qty"] = user_dic[key].get(f"{state}_pt_qty", 0) + \
-                                                           ceil(item['actual_trains'] / 12 * work_time)
+                                                           item['actual_trains'] / 12 * work_time
                         user_dic[key][f"{state}_pt_unit"] = unit
         results1 = {}
         # 是否独立上岗
