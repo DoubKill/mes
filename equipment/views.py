690571e9b9bf89da3ebb04b86fd2939dffbc25eb
kevin <771411855@qq.com>
2022-05-12 18:14:29 +0800

    [mod]增加工单验收搜索

--- equipment/views.py
+++ equipment/views.py
@@ -4101,34 +4101,50 @@ class EquipOrderListView(APIView):
     """
     根据机台和部位条码进行查询
     """
+
     def get(self, request):
         lot_no = self.request.query_params.get('lot_no')  # 扫描的条码
         search = self.request.query_params.get('search')
         my_order = self.request.query_params.get('my_order')
+        status = self.request.query_params.get('status')
         page = self.request.query_params.get('page', 1)
         page_size = self.request.query_params.get('page_size', 10)
         if lot_no:
             queryset1 = EquipApplyOrder.objects.filter(Q(equip_part_new__part_code=lot_no) | Q(equip_no=lot_no))
             queryset2 = EquipInspectionOrder.objects.filter(equip_no=lot_no)
         else:
-            queryset1 = EquipApplyOrder.objects.filter(Q(equip_part_new__part_code__icontains=search) | Q(equip_no__icontains=search))
+            queryset1 = EquipApplyOrder.objects.filter(
+                Q(equip_part_new__part_code__icontains=search) | Q(equip_no__icontains=search))
             queryset2 = EquipInspectionOrder.objects.filter(equip_no__icontains=search)
         if my_order:
-            queryset1 = queryset1.filter(Q(assign_user=self.request.user.username) |
-                                    Q(assign_to_user=self.request.user.username) |
-                                    Q(receiving_user=self.request.user.username) |
-                                    Q(accept_user=self.request.user.username) |
-                                    Q(status='已生成'))
-            queryset2 = queryset2.filter(Q(assign_user=self.request.user.username) |
-                                    Q(assign_to_user=self.request.user.username) |
-                                    Q(receiving_user=self.request.user.username) |
-                                    Q(status='已生成'))
+            if status == '已完成':
+                queryset1 = EquipApplyOrder.objects.filter(created_user__username__icontains=self.request.user.username,
+                                                           status=status)
+                queryset2 = []
+            elif status == '已验收':
+                queryset1 = EquipApplyOrder.objects.filter(accept_user__icontains=self.request.user.username,
+                                                           status=status)
+                queryset2 = []
+            else:
+                queryset1 = queryset1.filter(Q(assign_user=self.request.user.username) |
+                                             Q(assign_to_user=self.request.user.username) |
+                                             Q(receiving_user=self.request.user.username) |
+                                             Q(accept_user=self.request.user.username) |
+                                             Q(status='已生成'))
+                queryset2 = queryset2.filter(Q(assign_user=self.request.user.username) |
+                                             Q(assign_to_user=self.request.user.username) |
+                                             Q(receiving_user=self.request.user.username) |
+                                             Q(status='已生成'))
 
         serializer1 = EquipApplyOrderSerializer(instance=queryset1, many=True, context={'request': request}).data
-        serializer2 = EquipInspectionOrderSerializer(instance=queryset2, many=True, context={'request': request}).data
+        serializer = serializer1
+        if queryset2:
+            serializer2 = EquipInspectionOrderSerializer(instance=queryset2, many=True,
+                                                         context={'request': request}).data
+            serializer += serializer2
         st = (int(page) - 1) * int(page_size)
         et = int(page) * int(page_size)
-        data = sorted(serializer1 + serializer2, key=lambda x: x['created_date'], reverse=True)
+        data = sorted(serializer, key=lambda x: x['created_date'], reverse=True)
         return Response({'results': data[st:et], 'count': len(data)})
 
 
