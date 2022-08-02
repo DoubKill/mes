0fd89910a055d5abf749fc11883eacf872402608
kevin <771411855@qq.com>
2022-08-02 16:38:59 +0800

    [mod]删除标准的岗位隐藏

--- equipment/views.py
+++ equipment/views.py
@@ -5073,7 +5073,7 @@ class CheckPointStandardViewSet(ModelViewSet):
     def list(self, request, *args, **kwargs):
         all_station = self.request.query_params.get('all_station')  # 获取所有岗位
         if all_station:
-            station_list = list(CheckPointStandard.objects.values_list('station', flat=True).distinct())
+            station_list = list(self.queryset.values_list('station', flat=True).distinct())
             return Response({'results': station_list})
 
         queryset = self.filter_queryset(self.get_queryset())
