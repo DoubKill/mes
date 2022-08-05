779178edc79784d02815da0726bc17523daafda5
kevin <771411855@qq.com>
2022-08-05 14:25:27 +0800

    [mod]修改导出sheet_name名称

--- equipment/views.py
+++ equipment/views.py
@@ -5231,8 +5231,7 @@ class CheckPointTableViewSet(ModelViewSet):
             elif opera_type == 3:  # 导出
                 data = self.get_serializer(records, many=True).data
                 return gen_excels_response(self.EXPORT_FIELDS_DICT, data, self.FILE_NAME,
-                                           sheet_keyword=['select_date', 'classes', 'point_standard_name', 'equip_no',
-                                                          'station'],
+                                           sheet_keyword=['select_date', 'classes', 'equip_no', 'station'],
                                            handle_str=True)
             else:
                 raise ValidationError('异常: 未知操作类型')
