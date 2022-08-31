5fb1077c2c68dd80d22a5117d6cada43f743f08f
kevin <771411855@qq.com>
2022-08-31 11:50:43 +0800

    [mod]更新同备件同库位所有备注信息

--- equipment/views.py
+++ equipment/views.py
@@ -2855,7 +2855,8 @@ class EquipWarehouseInventoryViewSet(ModelViewSet):
     def create(self, request, *args, **kwargs):
         handle = self.request.data.get('handle')
         data = self.request.data
-        inventory = self.queryset.filter(equip_spare_id=data['equip_spare'], equip_warehouse_location_id=data['equip_warehouse_location__id']).first()
+        all_inventory = self.queryset.filter(equip_spare_id=data['equip_spare'], equip_warehouse_location_id=data['equip_warehouse_location__id'])
+        inventory = all_inventory.first()
         if not inventory:
             return Response({"success": False, "message": '备件代码不存在', "data": None})
         if handle:
@@ -2863,7 +2864,7 @@ class EquipWarehouseInventoryViewSet(ModelViewSet):
             if handle == '盘库':
                 quantity = data.get('quantity')
                 inventory.quantity = data['quantity']
-                inventory.check_desc = data.get('desc')
+                all_inventory.update(check_desc=data.get('desc'))
             elif handle == '移库':
                 if data['move_equip_warehouse_location__id'] == data['equip_warehouse_location__id']:
                     return Response({"success": False, "message": '不能移动到当前库区', "data": None})
@@ -2871,7 +2872,7 @@ class EquipWarehouseInventoryViewSet(ModelViewSet):
                 if inventory.quantity < data['quantity']:
                     return Response({"success": False, "message": '当前库存数量不足', "data": None})
                 inventory.quantity -= data['quantity']
-                inventory.move_desc = data.get('desc')
+                all_inventory.update(move_desc=data.get('desc'))
                 new_queryset = self.queryset.filter(equip_spare_id=data['equip_spare'], equip_warehouse_area_id=data['move_equip_warehouse_area__id'],
                                                     equip_warehouse_location_id=data['move_equip_warehouse_location__id'])
                 if new_queryset.exists():
