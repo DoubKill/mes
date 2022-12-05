9e6d7f4c6a5070de3fe0ff5288271a8cb139dcf3
kevin <771411855@qq.com>
2022-12-05 11:04:41 +0800

    [mod]撤销操作恢复未删除的库存

--- equipment/views.py
+++ equipment/views.py
@@ -3077,7 +3077,7 @@ class EquipWarehouseRecordViewSet(ModelViewSet):
         equip_spare = self.request.data.get('equip_spare')
         instance = self.get_object()
         quantity = round(float(instance.quantity), 1)
-        inventory = EquipWarehouseInventory.objects.filter(equip_spare_id=equip_spare,
+        inventory = EquipWarehouseInventory.objects.filter(equip_spare_id=equip_spare, delete_flag=False,
                                                            equip_warehouse_location_id=equip_warehouse_location).first()
         if instance.created_user == self.request.user:
             order_detail = instance.equip_warehouse_order_detail
