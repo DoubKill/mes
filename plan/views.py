f36b790585fe9f39fb93db3079eff5436838f14d
刘博文 <120@qq.com>
2022-12-19 19:18:36 +0800

    [mod] 排程导出修改，增加继承前一天计划数据；

--- plan/views.py
+++ plan/views.py
@@ -8,7 +8,7 @@ from operator import itemgetter
 import requests
 import xlrd
 from django.db import connection, IntegrityError
-from django.db.models import Sum, Max, Q
+from django.db.models import Sum, Max, Q, Min
 from django.db.transaction import atomic
 from django.http import HttpResponse
 from django.utils.decorators import method_decorator
@@ -1584,77 +1584,77 @@ class WeightPackageDailyTimeConsumeViewSet(ModelViewSet):
 @method_decorator([api_recorder], name='dispatch')
 class APSExportDataView(APIView):
 
-    def extend_last_aps_result(self, factory_date):
+    def extend_last_aps_result(self, factory_date, lock_durations):
         """
         继承前一天未打完的排程计划
+        @param lock_durations:
         @param factory_date: 日期
         @return:
         """
         yesterday = factory_date - datetime.timedelta(1)
-        yesterday_last_res = SchedulingResult.objects.filter(factory_date=yesterday).order_by('id').last()
         equip_plan_data = []
-        if yesterday_last_res:
-            for equip_no in Equip.objects.filter(
-                    category__equip_type__global_name='密炼设备').values_list('equip_no', flat=True).order_by('equip_no'):
-                query_set = SchedulingResult.objects.filter(
-                    schedule_no=yesterday_last_res.schedule_no,
-                    equip_no=equip_no)
-                if not query_set:
+        # 当天已经下达的计划，状态为commited
+        for plan in ProductClassesPlan.objects.filter(
+                work_schedule_plan__plan_schedule__day_time=factory_date,
+                delete_flag=False):
+            if plan.status in ('完成', '停止'):
+                trains_st_et = TrainsFeedbacks.objects.filter(
+                    plan_classes_uid=plan.plan_classes_uid
+                ).aggregate(st=Min('begin_time'), et=Max('end_time'))
+                st, et = trains_st_et['st'], trains_st_et['et']
+                if not st:
                     continue
-                # 找到昨天夜班最后一条计划，与该机台昨天排程的计划对比。
-                yesterday_plan = ProductClassesPlan.objects.using('SFJ').filter(
-                    equip__equip_no=equip_no,
-                    work_schedule_plan__plan_schedule__day_time=yesterday,
-                    status__in=('运行中', '完成', '停止')
-                ).order_by('-id').values('product_batching__stage_product_batch_no', 'plan_classes_uid')
-                if yesterday_plan:
-                    recipe_name = yesterday_plan[0]['product_batching__stage_product_batch_no']  # 配方名称
-                    plan_classes_uid = yesterday_plan[0]['plan_classes_uid']  # 计划编号
-                    q = query_set.filter(recipe_name=recipe_name).order_by('sn').last()
-                    if q:
-                        # 获取计划完成车次
-                        tfb_obj = TrainsFeedbacks.objects.using('SFJ').filter(
-                            plan_classes_uid=plan_classes_uid).order_by('-id').values('actual_trains')
-                        if tfb_obj:
-                            finished_trains = tfb_obj[0]['actual_trains']
-                        else:
-                            finished_trains = 0
+                time_consume = round((et - st).total_seconds() / 3600, 2)
+            else:
+                time_consume = round(
+                    calculate_equip_recipe_avg_mixin_time(
+                        plan.equip.equip_no,
+                        plan.product_batching.stage_product_batch_no
+                    ) * plan.plan_trains / 3600, 2)
+            equip_plan_data.append({'recipe_name': plan.product_batching.stage_product_batch_no,
+                                    'equip_no': plan.equip.equip_no,
+                                    'plan_trains': plan.plan_trains,
+                                    'time_consume': time_consume,
+                                    'status': 'COMMITED',
+                                    'delivery_time': time_consume,
+                                    })
 
-                        if finished_trains < q.plan_trains:
-                            unfinished_plan = query_set.filter(id__gte=q.id)
-                        else:
-                            unfinished_plan = query_set.filter(id__gt=q.id)
-                        for plan in unfinished_plan:
-                            if plan.id == q.id:
-                                plan_trains = plan.plan_trains - finished_trains
-                                time_consume = round(
-                                    calculate_equip_recipe_avg_mixin_time(equip_no,
-                                                                          plan.recipe_name) * plan_trains / 3600,
-                                    2)
-                            else:
-                                plan_trains = plan.plan_trains
-                                time_consume = plan.time_consume
-                            equip_plan_data.append({'recipe_name': plan.recipe_name,
-                                                    'equip_no': equip_no,
-                                                    'plan_trains': plan_trains,
-                                                    'time_consume': time_consume})
-        # equip_plan_data = [{'recipe_name': 'K-1MB-K310-000',
-        #                     'equip_no': 'Z01',
-        #                     'plan_trains': 10,
-        #                     'time_consume': 0.3},
-        #                    {'recipe_name': 'C-2MB-K305-02',
-        #                     'equip_no': 'Z02',
-        #                     'plan_trains': 10,
-        #                     'time_consume': 0.3},
-        #                    {'recipe_name': 'K-2MB-K310-000',
-        #                     'equip_no': 'Z03',
-        #                     'plan_trains': 3,
-        #                     'time_consume': 0.3},
-        #                    {'recipe_name': 'K-FM-K310-000',
-        #                     'equip_no': 'Z05',
-        #                     'plan_trains': 5,
-        #                     'time_consume': 0.3},
-        #                    ]
+        # 前一天未下达的计划，超过锁定时间以内的状态为COMMITED，其他为STANDARD
+        yesterday_last_res = SchedulingResult.objects.filter(
+            factory_date=yesterday).order_by('id').last()
+        if yesterday_last_res:
+            equip_time_dict = {}
+            last_aps_results = SchedulingResult.objects.filter(
+                schedule_no=yesterday_last_res.schedule_no).order_by('equip_no', 'sn').values()
+            for result in last_aps_results:
+                equip_no = result['equip_no']
+                recipe_name = result['recipe_name']
+                plan_trains = result['plan_trains']
+                time_consume = result['time_consume']
+                equip_time_consume = equip_time_dict.get(equip_no, 0) + time_consume
+                equip_time_dict[equip_no] = equip_time_consume
+                if equip_time_consume <= 24:
+                    continue
+                if result['status'] == '未下发':
+                    delivery_time = round(equip_time_consume - 24, 2)
+                    if delivery_time < time_consume:
+                        delivery_time = time_consume
+                    if equip_time_consume <= 24 + lock_durations:
+                        equip_plan_data.append({'recipe_name': recipe_name,
+                                                'equip_no': equip_no,
+                                                'plan_trains': plan_trains,
+                                                'delivery_time': delivery_time,
+                                                'time_consume': time_consume,
+                                                'status': 'COMMITED'
+                                                })
+                    else:
+                        equip_plan_data.append({'recipe_name': recipe_name,
+                                                'equip_no': equip_no,
+                                                'plan_trains': plan_trains,
+                                                'delivery_time': delivery_time,
+                                                'time_consume': time_consume,
+                                                'status': 'STANDARD'
+                                                })
         return equip_plan_data
 
     def get(self, request):
@@ -1685,8 +1685,6 @@ class APSExportDataView(APIView):
         sheet2 = wb.worksheets[2]
         job_list_data = {}  # 待排程结果数据 格式：{'C590-01': {'1MB': {},'2MB': {}}}
         pb_available_time_dict = {}  # 规格可用时间 格式： {'C590-01'：20, 'J290-01'：20}
-        data_row = 2
-        data_row1 = 2
 
         # 构建job_list_data数据
         for i in demanded_data:
@@ -1818,7 +1816,7 @@ class APSExportDataView(APIView):
                     job_list_data[pb_version_name] = {stage: {
                         'project_name': pb_version_name,
                         'stage': stage,
-                        # 'serial_no': need_stages.index(stage) + 1,
+                        'serial_no': need_stages.index(stage) + 1,
                         'job_name': recipe_name,
                         'job_type': 'STANDARD',
                         'weight_qty': weight,
@@ -1835,7 +1833,7 @@ class APSExportDataView(APIView):
                         job_list_data[pb_version_name][stage] = {
                             'project_name': pb_version_name,
                             'stage': stage,
-                            # 'serial_no': need_stages.index(stage) + 1,
+                            'serial_no': need_stages.index(stage) + 1,
                             'job_name': recipe_name,
                             'job_type': 'STANDARD',
                             'weight_qty': weight,
@@ -1861,97 +1859,28 @@ class APSExportDataView(APIView):
             if pb_version_name not in job_list_data:
                 raise ValidationError('该规格启用配方未找到:{}'.format(pb_version_name))
 
-        # 构建前一天未完成计划数据 格式：{'C590-01': {'1MB': [{}, {}],'2MB': [{}, {}]}}
-        job_left_list_data = {}
-        left_plans = self.extend_last_aps_result(datetime.datetime.strptime(factory_date, "%Y-%m-%d"))
-        for plan in left_plans:
-            recipe_name = plan['recipe_name']
-            plan_trains = plan['plan_trains']
-            time_consume = plan['time_consume']
-            equip_no = plan['equip_no']
-            try:
-                pb_version_name = '-'.join(recipe_name.split('-')[-2:])
-                stage = recipe_name.split('-')[1]
-            except Exception:
-                continue
-            if pb_version_name not in job_left_list_data:
-                job_left_list_data[pb_version_name] = {stage: [{
-                    'project_name': pb_version_name,
-                    'stage': stage,
-                    'job_name': recipe_name,
-                    'job_type': 'COMMITED',
-                    'weight_qty': 999,
-                    'model_size': 1,
-                    'details': [{'time_consume': time_consume * 60,
-                                 'equip_no': int(equip_no[-2:]),
-                                 'wait_time': int(time_consume / plan_trains * 60 * sps.scheduling_interval_trains),
-                                 'plan_trains': plan_trains,
-                                 'main_equip_flag': 1
-                                 }]}]
-                }
-            else:
-                if stage not in job_left_list_data[pb_version_name]:
-                    job_left_list_data[pb_version_name][stage] = [{
-                        'project_name': pb_version_name,
-                        'stage': stage,
-                        'job_name': recipe_name,
-                        'job_type': 'COMMITED',
-                        'weight_qty': 999,
-                        'model_size': 1,
-                        'details': [{'time_consume': time_consume * 60,
-                                     'equip_no': int(equip_no[-2:]),
-                                     'wait_time': int(time_consume / plan_trains * 60 * sps.scheduling_interval_trains),
-                                     'plan_trains': plan_trains,
-                                     'main_equip_flag': 1
-                                     }]}]
-                else:
-                    job_left_list_data[pb_version_name][stage].append(
-                        {
-                            'project_name': pb_version_name,
-                            'stage': stage,
-                            'job_name': recipe_name,
-                            'job_type': 'COMMITED',
-                            'weight_qty': 999,
-                            'model_size': 1,
-                            'details': [{'time_consume': time_consume * 60,
-                                         'equip_no': int(equip_no[-2:]),
-                                         'wait_time': int(
-                                             time_consume / plan_trains * 60 * sps.scheduling_interval_trains),
-                                         'plan_trains': plan_trains,
-                                         'main_equip_flag': 1
-                                         }]}
-                    )
+        left_plans = self.extend_last_aps_result(datetime.datetime.strptime(factory_date, "%Y-%m-%d"),
+                                                 sps.lock_durations)
 
         # write excel
-        id_st = 1
+        data_row = 2
+        data_row1 = 2
         # 待排程和前一天未完成的所有胶料规格数据 ['C590-01', 'J290-01']
-        pb_version_names = set(list(job_list_data.keys()) + list(job_left_list_data.keys()))
-        sheet.cell(8, 2).value = len(pb_version_names)  # 需要排程的规格数量
+        sheet.cell(8, 2).value = len(list(job_list_data.keys())) + len(left_plans)   # 需要排程的规格数量
 
         # 结合待排程和未完成计划，写入excel
-        for pb_name in pb_version_names:
+        for pb_name, item in job_list_data.items():
             sheet1.cell(data_row, 1).value = data_row - 1  # 序号
             sheet1.cell(data_row, 2).value = pb_name  # 规格名称（带版本号）
             sheet1.cell(data_row, 3).value = 0  # 胶料代码开始时间(暂时无用)
             sheet1.cell(data_row, 4).value = 0  # 关键路径持续时间（暂时无用）
             sheet1.cell(data_row, 5).value = pb_available_time_dict.get(pb_name, 720)
-            left_data = job_left_list_data.get(pb_name, {})
-            job_data = job_list_data.get(pb_name, {})
-            job_lists = []
-            stage_list = sorted(list(set(list(left_data.keys()) + list(job_data.keys()))),
-                                key=lambda x: idx_keys.get(x, 999))
-            sheet1.cell(data_row, 6).value = len(stage_list)  # job_list_size(总共需要打待段次数量)
-            data_row += 1
-            for s in stage_list:
-                if s in job_data:
-                    job_lists.append(job_data[s])
-                if s in left_data:
-                    job_lists += left_data[s]
-            for data in job_lists:
+            sheet1.cell(data_row, 6).value = len(item)  # job_list_size(总共需要打待段次数量)
+            for _, data in item.items():
                 # 写入job_list sheet
-                sheet2.cell(data_row1, 1).value = id_st
+                sheet2.cell(data_row1, 1).value = data_row - 1
                 sheet2.cell(data_row1, 2).value = data['project_name']
-                sheet2.cell(data_row1, 3).value = stage_list.index(data['stage']) + 1
+                sheet2.cell(data_row1, 3).value = data['serial_no']
                 sheet2.cell(data_row1, 4).value = data['job_name']
                 sheet2.cell(data_row1, 5).value = data['job_type']
                 sheet2.cell(data_row1, 6).value = data['weight_qty']
@@ -1965,8 +1894,30 @@ class APSExportDataView(APIView):
                     sheet2.cell(data_row1, data_col1+3).value = int(d['plan_trains'])
                     data_col1 += 4
                 data_row1 += 1
-            id_st += 1
+            data_row += 1
 
+        for j in left_plans:
+            # 写入project list
+            sheet1.cell(data_row, 1).value = data_row - 1  # 序号
+            sheet1.cell(data_row, 2).value = '-'.join(j['recipe_name'].split('-')[-2:])  # 规格名称（带版本号）
+            sheet1.cell(data_row, 3).value = 0  # 胶料代码开始时间(暂时无用)
+            sheet1.cell(data_row, 4).value = 0  # 关键路径持续时间（暂时无用）
+            sheet1.cell(data_row, 5).value = int(j['delivery_time'] * 60)
+            sheet1.cell(data_row, 6).value = 1  # job_list_size(总共需要打待段次数量)
+            # 写入job list
+            sheet2.cell(data_row1, 1).value = data_row - 1
+            sheet2.cell(data_row1, 2).value = '-'.join(j['recipe_name'].split('-')[-2:])
+            sheet2.cell(data_row1, 3).value = 1
+            sheet2.cell(data_row1, 4).value = j['recipe_name']
+            sheet2.cell(data_row1, 5).value = j['status']
+            sheet2.cell(data_row1, 6).value = 8.88
+            sheet2.cell(data_row1, 7).value = 1
+            sheet2.cell(data_row1, 8).value = int(j['time_consume']*60)
+            sheet2.cell(data_row1, 9).value = int(j['equip_no'][-2:])
+            sheet2.cell(data_row1, 10).value = int(j['time_consume']/j['plan_trains'] * 60 * sps.scheduling_interval_trains)
+            sheet2.cell(data_row1, 11).value = int(j['plan_trains'])
+            data_row1 += 1
+            data_row += 1
         # machine stop plan sheet
         sheet3 = wb.worksheets[3]
         data_row2 = 2
